0ee8c5f1163604968acfd9b779dcb9138249ff5b
Philip Vieira <philipvieira00@gmail.com>
2019-08-27 19:41:40 -0400

    Removed example.js and replaced it with landmark.js

---
+++ models/landmark.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+  var Landmark = sequelize.define("Landmark", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    }
+  });
+  return Landmark;
+};
