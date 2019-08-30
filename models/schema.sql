e40c98095eb0e6b1f0855e7df8d332d281dfd957
Ryan Lester <lestertr@dukes.jmu.edu>
2019-08-29 20:24:22 -0400

    added 404 error HTML page and created most of parks table in schema

--- models/schema.sql
+++ models/schema.sql
@@ -2,3 +2,16 @@
 DROP DATABASE IF EXISTS nat_parks_db;
 -- Creates the "nat_parks_db" database --
 CREATE DATABASE nat_parks_db;
+USE nat_parks_db;
+
+CREATE TABLE parks
+(
+    id int NOT NULL AUTO_INCREMENT,
+	name varchar(100) NOT NULL,
+    latitude DECIMAL(8,6),
+    longitude DECIMAL(8,6),
+    /*park_id
+    description
+    PRIMARY KEY(id)*/
+
+)
