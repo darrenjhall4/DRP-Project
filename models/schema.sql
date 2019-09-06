ea1f3490a4888591073ea378b6e78163fad3225c
Ryan Lester <lestertr@dukes.jmu.edu>
2019-09-05 20:15:49 -0400

    data injected into mySQL

--- models/schema.sql
+++ models/schema.sql
@@ -6,12 +6,13 @@ USE nat_parks_db;
 
 CREATE TABLE parks
 (
-    id int NOT NULL AUTO_INCREMENT,
-	name varchar(100) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+	park_name varchar(100) NOT NULL,
+    park_location varchar(255) NOT NULL,
     latitude DECIMAL(8,6),
     longitude DECIMAL(8,6),
-    /*park_id
-    description
-    PRIMARY KEY(id)*/
+    PRIMARY KEY(id)
 
-)
+);
+
+SELECT * from parks;
